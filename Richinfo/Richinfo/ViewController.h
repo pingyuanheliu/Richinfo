9b4e5e2be4c1e1c528200ed383bd58a2b145906d
LL <ll@LLdeiMac.local>
2018-09-07 10:34:35 +0800

    1、初始工程；

---
+++ Richinfo/Richinfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Richinfo
+//
+//  Created by LL on 2018/9/7.
+//  Copyright © 2018年 彩讯科技股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Richinfo/Richinfo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Richinfo
+//
+//  Created by LL on 2018/9/7.
+//  Copyright © 2018年 彩讯科技股份有限公司. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
